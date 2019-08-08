4477ba5d7eec5bfbe13e2ef89116119bf6de61d0
tungyao <1922527051@qq.com>
2019-08-08 18:13:47 +0800

    Writing Pool

--- test/db_test.go
+++ test/db_test.go
@@ -8,6 +8,7 @@ import (
 var db tndb.FUNC = new(tndb.DB).Use("tung_db")
 
 func TestDb(t *testing.T) {
-	db.Insert("t_user").Key([]string{`NULL`, "123123", `NULL`}).Done()
-
+	//db.Insert("t_user").Key([]string{`NULL`, "123123", `NULL`}).Done()
+	//db.Update("t_user").Key(map[string]string{"email":"asdasdas"}).Where(map[string]string{"id":"10"}).Done()
+	db.Select("t_user").FindOne("id", "email")
 }
--- tndb/dbcore.go
+++ tndb/dbcore.go
@@ -2,7 +2,6 @@ package tndb
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -10,23 +9,98 @@ const (
 	INSERT = iota
 	UPDATE
 	DELETE
+	Select
 )
 
 type FUNC interface {
 	Use(string) *DB
+
+	Select(string) *DB
+	All() *DB
+	FindOne(column ...interface{}) *DB
+	Count() *DB
+	IsExits() *DB
+
 	Insert(string) *DB
 	Update(string) *DB
 	Key(interface{}) *DB
 	Where(map[string]string) *DB
-	Done() bool
+	Done() int64
 }
 
 type DB struct {
-	op    int
-	db    string
-	table string
-	sql   string
-	kel   *sql.DB
+	op        int
+	db        string
+	table     string
+	sql       string
+	kel       *sql.DB
+	formatSql [4]string
+}
+
+func (d *DB) Select(tablename string) *DB {
+	d.op = Select
+	d.formatSql[0] = "select"
+	d.formatSql[2] = "from " + tablename
+
+	return d
+}
+
+func (d *DB) All() *DB {
+	d.formatSql[1] = " * "
+
+	return d
+}
+
+type data struct {
+	id    int
+	email string
+}
+
+func (d *DB) FindOne(column ...interface{}) *DB {
+	var tos string
+	for _, v := range column {
+		tos += string(v.(string)) + ","
+
+	}
+	d.formatSql[1] = " " + tos[:len(tos)-1] + " "
+	d.formatSql[3] = " limit 1"
+	for _, v := range d.formatSql {
+		d.sql += v
+	}
+
+	rows, err := d.kel.Query(d.sql)
+	toError(err)
+	columns, _ := rows.Columns()
+	length := len(columns)
+	for rows.Next() {
+		value := make([]interface{}, length)
+		columnPointers := make([]interface{}, length)
+		for i := 0; i < length; i++ {
+			columnPointers[i] = &value[i]
+		}
+		rows.Scan(columnPointers...)
+		data := make(map[string]interface{})
+		for i := 0; i < length; i++ {
+			columnName := columns[i]
+			columnValue := columnPointers[i].(*interface{})
+			data[columnName] = *columnValue
+		}
+	}
+	return d
+}
+
+func B2S(bs []uint8) string {
+	ba := []byte{}
+	for _, b := range bs {
+		ba = append(ba, byte(b))
+	}
+	return string(ba)
+}
+func (d *DB) Count() *DB {
+	return d
+}
+func (d *DB) IsExits() *DB {
+	return d
 }
 
 //TODO 使用数据库 Use
@@ -72,11 +146,16 @@ func (d *DB) Where(key map[string]string) *DB {
 }
 
 //TODO 数据
-func (d *DB) Done() bool {
-	stmt, err := d.kel.Prepare(d.sql)
-	fmt.Println(err)
-	res, _ := stmt.Exec()
-	id, _ := res.LastInsertId()
-	fmt.Println(id)
-	return true
+func (d *DB) Done() int64 {
+
+	stmt, _ := d.kel.Prepare(d.sql)
+	res, err := stmt.Exec()
+	toError(err)
+	id, err := res.LastInsertId()
+	toError(err)
+	return id
+
+}
+func toError(err error) {
+	return
 }
