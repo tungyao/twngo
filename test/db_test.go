c6ad52dcbaa58b0fd818ace575c944fb20dc7a67
tungyao <1922527051@qq.com>
2019-08-10 18:01:14 +0800

    修改findone

--- test/db_test.go
+++ test/db_test.go
@@ -12,6 +12,6 @@ var db tndb.FUNC = new(tndb.DB).Use("tung_db", "123456")
 func TestDb(t *testing.T) {
 	//db.Insert("user").Key([]string{`NULL`, "123123123", `NULL`}).Done()
 	//db.Update("t_user").Key(map[string]string{"email":"asdasdas"}).Where(map[string]string{"id":"10"}).Done()
-	data := db.Select("user").All("id", "email")
-	fmt.Println(tndb.B2S(data))
+	data := db.Select("user").FindOne("id", "email")
+	fmt.Println(data)
 }
--- tnwb/route.go
+++ tnwb/route.go
@@ -1,52 +1,74 @@
 package tnwb
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const sonNum = 52
 const base = 'a'
 
 type Trie struct {
 	num  int64
-	node *Son
+	root *Son
 }
 type Son struct {
 	key      string
+	path     string
 	value    string
 	deep     int
-	child    map[rune]*Son
+	child    map[string]*Son
 	terminal bool
 }
 
-func NewSon(key string, value string, deep int) *Son {
+func NewSon(path string, value string, deep int) *Son {
 	return &Son{
-		key:   key,
+		key:   path,
 		value: value,
+		path:  path,
 		deep:  deep,
-		child: make(map[rune]*Son),
+		child: make(map[string]*Son),
 	}
 }
-func NewTree() *Trie {
+func NewTrie() *Trie {
 	return &Trie{
-		num:  0,
-		node: NewSon("k", "kkk", 1),
+		num:  1,
+		root: NewSon("a", "value->a", 1),
 	}
 }
-func (t *Trie) Insert(key string, value string) {
-	var currentNode = t.node
-	if key != currentNode.key {
-		for _, v := range key {
-			son, ok := currentNode.child[v]
-			fmt.Println(v, ok)
-			if !ok {
-				son = NewSon(key, value, currentNode.deep+1)
-				currentNode.child[v] = son
+func (t *Trie) Insert(path string, value string) {
+	son := t.root
+	res := strings.Split(path, "")
+	if son.key != path { //匹配不成功才加入链表
+		for _, key := range res {
+			node, ok := son.child[key] //看字son中存不存在该 key
+			if !ok {                   //不存在就加进去
+				node = NewSon(key, value, son.deep+1) //这个其实是son的son
 			}
-			currentNode = son
+			son.child[key] = node
 
 		}
 	}
-	currentNode.value = value
-	currentNode.terminal = true
-	t.node = currentNode
-	fmt.Println(t.node)
+	son.terminal = true
+	son.value = value
+	son.path = path
+	t.root = son
+}
+func (t *Trie) Find(key string) {
+	son := t.root
+	res := strings.Split(key, "")
+	if son.key != key { //匹配不成功才加入链表
+		for _, key := range res {
+			node, ok := son.child[key]
+			fmt.Println(node)
+
+			if ok {
+				son = node
+				fmt.Println("找到：", node.key, key)
+			} else {
+				fmt.Println("没找到", key)
+				return
+			}
+		}
+	}
 }
